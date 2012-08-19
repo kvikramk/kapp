9cce023835e0b1b628182a7e879a7a23168ba7ee
vikram <kvikraminsti@gmail.com>
2012-08-19 15:29:48 +0530

    layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,36 +5,48 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_content('Sample App')
     end
     
     it "should have the right title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title',:text=>"Ruby on Rails Tutorial Sample App | Home")
     end
   end
   
   describe "Help Page" do
     it "shoud have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_content('Help')
     end
     
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title',:text=>"Ruby on Rails Tutorial Sample App | Help")
     end
   end
   
   describe "About Page" do
     it "should have content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_content('About Us')
     end
     it "should have the right title" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title',:text=>"Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
+  
+  describe "Contact Page" do
+    it "should have h1  'Contact" do
+      visit contact_path
+      page.should have_selector('h1',text:'Contact')
+    end
+    
+    it "should have title 'Contact" do
+      visit contact_path
+      page.should have_selector('title',text:"Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 end
